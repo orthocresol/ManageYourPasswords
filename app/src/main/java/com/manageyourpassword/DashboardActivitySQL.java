b8dc48cfa7bef20a250dbfe2c8fa10719937325a
asifahmed <asifahmed.mit@gmail.com>
2021-06-18 03:59:43 +0600

    Password Generator Added

--- app/src/main/java/com/manageyourpassword/DashboardActivitySQL.java
+++ app/src/main/java/com/manageyourpassword/DashboardActivitySQL.java
@@ -3,6 +3,7 @@ package com.manageyourpassword;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,7 +11,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DashboardActivitySQL extends AppCompatActivity {
     FloatingActionButton floatingActionButton;
-
+    Button temporary_generator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +20,20 @@ public class DashboardActivitySQL extends AppCompatActivity {
         setTitle("Dashboard");
 
         floatingActionButton = findViewById(R.id.dashboard_floating_button);
+        temporary_generator = findViewById(R.id.dashboard_generator);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DashboardActivitySQL.this, AddItemActivitySQL.class);
+                startActivity(intent);
+            }
+        });
+
+        temporary_generator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DashboardActivitySQL.this, GeneratorActivity.class);
                 startActivity(intent);
             }
         });
